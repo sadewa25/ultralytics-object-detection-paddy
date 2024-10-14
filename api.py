f3058d1bbc29fe826d101d33632b07aa53b2e60a
root <root@root.jagopadi.com>
2024-10-14 00:39:05 +0000

    origin header

--- api.py
+++ api.py
@@ -9,8 +9,23 @@ app = FastAPI()
 
 app.mount("/results", StaticFiles(directory="results"), name="results")
 
-# UPLOAD_DIR = "/root/ultralytics/ultralytics-object-detection-paddy/files"
-UPLOAD_DIR = "/Users/sadewawicak/Project/JagoPadi/ultralytics-object-detection-paddy/files"
+# Configure CORS
+origins = [
+    "https://jagopadi.com",
+    "https://sidito.my.id",
+    # Add other origins as needed
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+UPLOAD_DIR = "/root/ultralytics/ultralytics-object-detection-paddy/files"
+# UPLOAD_DIR = "/Users/sadewawicak/Project/JagoPadi/ultralytics-object-detection-paddy/files"
 
 @app.post("/upload/")
 async def upload_file(file: UploadFile = File(...)):
@@ -47,3 +62,6 @@ async def get_result(task_id: str):
 # celery -A  celery_config.celery_app worker --loglevel=info --concurrency=1
 # celery -A  celery_config.celery_app flower --host 0.0.0.0 --loglevel=info
 # 192.168.1.20:8000/results/c29fac9b-e633-45e9-a942-5270615a3c6e_My Video.mp4.avi
+# http://103.16.117.163:5555/tasks
+# http://103.16.117.163:8000/results/80ca3475-0cd7-4f1f-81fd-17375e511b1e_My%20Video.mp4.avi
+# track-uvicorn.service
